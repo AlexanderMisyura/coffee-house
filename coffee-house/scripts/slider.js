89280a22e4c6be6ec068b084a2af757ea9c6a582
Alexander Misyura <alexanderpinsk@gmail.com>
2023-12-12 01:50:16 +0300

    feat: add manual carousel switch with buttons

--- coffee-house/scripts/slider.js
+++ coffee-house/scripts/slider.js
@@ -2,8 +2,8 @@ slider();
 
 function slider() {
   const slider = document.querySelector(".slider");
-  const left = slider.querySelector(".slider__btn-left");
-  const right = slider.querySelector(".slider__btn-right");
+  const leftBtn = slider.querySelector(".slider__btn-left");
+  const rightBtn = slider.querySelector(".slider__btn-right");
   const row = slider.querySelector(".slider__active-content");
 
   const sliderItems = [...row.children];
@@ -13,44 +13,58 @@ function slider() {
   let nextIndex = activeIndex + 1;
 
   const sliderControls = [...slider.querySelectorAll(".slider__control")];
-  [sliderControls[1], sliderControls[2]] = [sliderControls[2], sliderControls[1]];
+  [sliderControls[1], sliderControls[2]] = [
+    sliderControls[2],
+    sliderControls[1],
+  ];
   sliderControls[0].classList.add("slider__control_active");
 
   let timerId;
+  // let timestamp;
 
-  timerId = setInterval(() => {
-    prevIndex = activeIndex + 1 > sliderLength - 1 ? 0 : activeIndex + 1;
-    activeIndex = activeIndex - 1 < 0 ? sliderLength - 1 : activeIndex - 1;
-
-    console.log('activeIndex', activeIndex)
-
+  function moveCarousel() {
     sliderItems.forEach((item, i) => {
       if (i === activeIndex) {
         item.className = "";
         item.classList.add("slider__item", "slider__item_active");
-        sliderControls[i].classList.add(
-          "slider__control_active"
-        );
+        sliderControls[i].classList.add("slider__control_active");
       } else if (i === prevIndex) {
         item.className = "";
         item.classList.add("slider__item", "slider__item_prev");
         sliderControls[i].classList.remove("slider__control_active");
-        sliderControls
+        sliderControls;
       } else {
         item.className = "";
         item.classList.add("slider__item", "slider__item_next");
         sliderControls[i].classList.remove("slider__control_active");
       }
     });
+  }
 
-    // sliderControls.forEach((control, i) => {
-    //   if (i === activeIndex) {
-    //     control.className = "";
-    //     control.classList.add("slider__control", "slider__control_active");
-    //   } else {
-    //     control.className = "";
-    //     control.classList.add("slider__control");
-    //   }
-    // })
-  }, 6000);
+  startInterval();
+
+  function startInterval() {
+    timerId = setInterval(() => {
+      activeIndex = activeIndex - 1 < 0 ? sliderLength - 1 : activeIndex - 1;
+      prevIndex = activeIndex + 1 > sliderLength - 1 ? 0 : activeIndex + 1;
+
+      moveCarousel();
+    }, 6000);
+  }
+
+  rightBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    activeIndex = activeIndex - 1 < 0 ? sliderLength - 1 : activeIndex - 1;
+    prevIndex = activeIndex + 1 > sliderLength - 1 ? 0 : activeIndex + 1;
+    moveCarousel();
+    startInterval();
+  });
+
+  leftBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    activeIndex = activeIndex + 1 > sliderLength - 1 ? 0 : activeIndex + 1;
+    prevIndex = activeIndex + 1 > sliderLength - 1 ? 0 : activeIndex + 1;
+    moveCarousel();
+    startInterval();
+  });
 }
